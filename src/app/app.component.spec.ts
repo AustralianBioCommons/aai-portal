508bbfc17e21302396c332d3d331884cb989c7c2
marius-mather <marius.mather@sydney.edu.au>
2025-04-01 13:32:19 +1100

    Update test of app component: check navbar has been created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should create the navbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aai-portal');
+    expect(compiled.querySelector("nav")).toBeDefined();
   });
 });
