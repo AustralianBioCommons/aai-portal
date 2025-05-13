c64d7c9e539aa14ae271885d0714db5516e177d0
marius-mather <marius.mather@sydney.edu.au>
2025-05-13 13:06:01 +1000

    Add layout for galaxy-related pages

---
+++ src/app/layouts/galaxy-layout/galaxy-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-galaxy-layout',
+  imports: [RouterOutlet],
+  templateUrl: './galaxy-layout.component.html',
+  styleUrl: './galaxy-layout.component.css',
+})
+export class GalaxyLayoutComponent {}
