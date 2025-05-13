88320bcd842d9c56617aea7ccb9bd36aa4199b11
marius-mather <marius.mather@sydney.edu.au>
2025-05-13 11:29:22 +1000

    Add galaxy registration to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './pages/shared/not-found/not-found.component'
 import { ListUsersComponent } from './pages/admin/list-users/list-users.component';
 import { RequestsComponent } from './pages/admin/requests/requests.component';
 import { RevokedComponent } from './pages/admin/revoked/revoked.component';
+import { RegisterComponent } from './pages/galaxy/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/services', pathMatch: 'full' },
@@ -25,5 +26,6 @@ export const routes: Routes = [
   { path: 'all-users', component: ListUsersComponent },
   { path: 'revoked', component: RevokedComponent },
   { path: 'requests', component: RequestsComponent },
+  { path: 'galaxy/register', component: RegisterComponent},
   { path: '**', component: NotFoundComponent },
 ];
