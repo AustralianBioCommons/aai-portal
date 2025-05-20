4b1a9cf58f14e3832048c0bb47c77829bd836398
marius-mather <marius.mather@sydney.edu.au>
2025-05-20 11:52:16 +1000

    Add registration success page for Galaxy reg

---
+++ src/app/pages/galaxy/register-success/galaxy-register-success.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GalaxyRegisterSuccessComponent } from './galaxy-register-success.component';
+
+describe('RegisterSuccessComponent', () => {
+  let component: GalaxyRegisterSuccessComponent;
+  let fixture: ComponentFixture<GalaxyRegisterSuccessComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GalaxyRegisterSuccessComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GalaxyRegisterSuccessComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
