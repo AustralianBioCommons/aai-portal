31f12e38d645b4bfab296359a78c14cdca53fa3d
Minh Vu <minh@biocommons.org.au>
2025-06-05 12:02:28 +1000

    Update backend URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
     redirectUri: window.location.origin,
     audience: 'https://dev-bc.au.auth0.com/api/v2/',
     scope: 'read:current_user update:current_user_metadata',
-    backend: 'http://localhost:8000',
+    backend: 'https://aaibackend.test.biocommons.org.au',
   },
 };
