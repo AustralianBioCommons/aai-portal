138e60c6e99fd85d3bb22e2cb475fb7747a0b7a5
Minh Vu <minh@biocommons.org.au>
2025-06-23 15:45:56 +1000

    responsive

--- src/app/pages/galaxy/register-selection/galaxy-register-selection.component.ts
+++ src/app/pages/galaxy/register-selection/galaxy-register-selection.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-selection',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './galaxy-register-selection.component.html',
   styleUrl: './galaxy-register-selection.component.css',
 })
