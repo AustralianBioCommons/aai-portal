e981f549dcc655b406ee58a7fed241d241125766
Minh Vu <minh@biocommons.org.au>
2025-06-24 10:16:13 +1000

    update bpa favicon

--- src/app/pages/bpa/register-selection/bpa-register-selection.component.ts
+++ src/app/pages/bpa/register-selection/bpa-register-selection.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-register-selection',
@@ -7,4 +8,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './bpa-register-selection.component.html',
   styleUrls: ['./bpa-register-selection.component.css'],
 })
-export class BpaRegisterSelectionComponent {}
+export class BpaRegisterSelectionComponent {
+  private document = inject(DOCUMENT);
+  private defaultFavicon: string | null = null;
+
+  ngOnInit(): void {
+    this.defaultFavicon =
+      this.document.querySelector("link[rel*='icon']")?.getAttribute('href') ||
+      null;
+    this.setFavicon('/assets/bpa-favicon.ico');
+  }
+
+  ngOnDestroy(): void {
+    if (this.defaultFavicon) {
+      this.setFavicon(this.defaultFavicon);
+    }
+  }
+
+  private setFavicon(href: string): void {
+    const links = this.document.querySelectorAll("link[rel*='icon']");
+    links.forEach((link) => link.setAttribute('href', href));
+  }
+}
