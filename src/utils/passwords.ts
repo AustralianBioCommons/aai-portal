4d28e56815b950bf82ca0bf0db23fda7530e19b4
marius-mather <marius.mather@sydney.edu.au>
2025-07-25 10:19:24 +1000

    Password validation functions

---
+++ src/utils/passwords.ts
@@ -0,0 +1,25 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export const ALLOWED_SPECIAL_CHARACTERS = '!@#$%^&*';
+
+export function lowercaseRequired(control: AbstractControl): ValidationErrors | null {
+  const ok = /[a-z]/.test(control.value);
+  return ok ? null : {lowercaseRequired: true};
+}
+
+export function uppercaseRequired(control: AbstractControl): ValidationErrors | null {
+  const ok = /[A-Z]/.test(control.value);
+  return ok ? null : {uppercaseRequired: true};
+}
+
+export function digitRequired(control: AbstractControl): ValidationErrors | null {
+  const ok = /[0-9]/.test(control.value);
+  return ok ? null : {digitRequired: true};
+}
+
+export function specialCharacterRequired(control: AbstractControl): ValidationErrors | null {
+  const regex = new RegExp(`[${ALLOWED_SPECIAL_CHARACTERS}]`);
+  const ok = regex.test(control.value);
+  return ok ? null : {specialCharacterRequired: true};
+}
+
