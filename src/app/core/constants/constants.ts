5fb9ac4dfafafdebc9d9144907b9f0fd59628edd
Minh Vu <minh@biocommons.org.au>
2025-07-31 09:53:16 +1000

    refactor: remove unused servicesList constant for cleaner code

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,26 +1,3 @@
-export const servicesList = [
-  {
-    id: 'TSI_SH',
-    name: 'Threatened Species Initiative Service Hub',
-    url: 'https://threatenedspeciesinitiative.com',
-  },
-  {
-    id: 'BPA_DP',
-    name: 'Bioplatform Data Portal',
-    url: 'https://data.bioplatforms.com',
-  },
-  {
-    id: 'GX_AU',
-    name: 'Galaxy Australia',
-    url: 'https://usegalaxy.org.au',
-  },
-  {
-    id: 'SBP',
-    name: 'Structural Biology Platform',
-    url: 'https://seqera.services.biocommons.org.au/login',
-  },
-];
-
 export interface BundleService {
   id: string;
   name: string;
