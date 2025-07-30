b61b91dc9ec47a480b60dc779cf033eb3a3f1bb2
Minh Vu <minh@biocommons.org.au>
2025-07-31 09:51:52 +1000

    refactor: move bundle services and constants to a dedicated file for better organization

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -20,3 +20,74 @@ export const servicesList = [
     url: 'https://seqera.services.biocommons.org.au/login',
   },
 ];
+
+export interface BundleService {
+  id: string;
+  name: string;
+  termsUrl: string;
+}
+
+export interface Bundle {
+  id: string;
+  name: string;
+  description: string;
+  logoUrls: string[];
+  listItems: string[];
+  disabled?: boolean;
+  services: BundleService[];
+}
+
+const bpaBundleService: BundleService = {
+  id: 'bpa',
+  name: 'Bioplatforms Australia Data Portal Terms and Conditions',
+  termsUrl: 'https://data.bioplatforms.com/',
+};
+
+const galaxyBundleService: BundleService = {
+  id: 'galaxy',
+  name: 'Galaxy Australia Terms of Service',
+  termsUrl: 'https://site.usegalaxy.org.au/about#terms-of-service',
+};
+
+const tsiBundleService: BundleService = {
+  id: 'tsi',
+  name: 'TSI Terms and Conditions',
+  termsUrl: 'https://threatenedspeciesinitiative.com/',
+};
+
+export const BUNDLES: Bundle[] = [
+  {
+    id: 'data-portal-galaxy',
+    name: 'Data Portal and Galaxy',
+    description: 'Bundle includes:',
+    logoUrls: ['/assets/bpa-logo.png', '/assets/galaxy-logo.png'],
+    listItems: [
+      'The <span class="font-medium">Data Portal</span> public data access',
+      'The <span class="font-medium">Galaxy Australia</span> data access',
+      'Easily import data into Galaxy from the Data Portal',
+    ],
+    services: [bpaBundleService, galaxyBundleService],
+  },
+  {
+    id: 'tsi',
+    name: 'Threatened Species Initiative',
+    description: 'Bundle includes:',
+    logoUrls: ['/assets/tsi-logo.jpg'],
+    listItems: [
+      'The <span class="font-medium">Data Portal</span> and <span class="font-medium">Galaxy Australia</span> data access',
+      'TSI public and restricted datasets in the Data Portal',
+      'Galaxy Australia 2TB storage space, high memory nodes, workflows and tools',
+      'Easily import data into Galaxy from the Data Portal',
+    ],
+    services: [tsiBundleService, bpaBundleService, galaxyBundleService],
+  },
+  {
+    id: 'fungi',
+    name: "Fungi Functional 'Omics",
+    description: '',
+    logoUrls: ['/assets/aff-logo.png'],
+    listItems: [],
+    disabled: true,
+    services: [],
+  },
+];
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,22 +10,7 @@ import {
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
-
-interface BundleService {
-  id: string;
-  name: string;
-  termsUrl: string;
-}
-
-interface Bundle {
-  id: string;
-  name: string;
-  description: string;
-  logoUrls: string[];
-  listItems: string[];
-  disabled?: boolean;
-  services: BundleService[];
-}
+import { BUNDLES, Bundle } from '../../core/constants/constants';
 
 interface RegistrationForm {
   firstName: FormControl<string>;
@@ -51,64 +36,7 @@ export class RegisterComponent {
   currentStep = 1;
   totalSteps = 5;
 
-  private readonly bpaBundleService: BundleService = {
-    id: 'bpa',
-    name: 'Bioplatforms Australia Data Portal Terms and Conditions',
-    termsUrl: 'https://data.bioplatforms.com/',
-  };
-
-  private readonly galaxyBundleService: BundleService = {
-    id: 'galaxy',
-    name: 'Galaxy Australia Terms of Service',
-    termsUrl: 'https://site.usegalaxy.org.au/about#terms-of-service',
-  };
-
-  private readonly tsiBundleService: BundleService = {
-    id: 'tsi',
-    name: 'TSI Terms and Conditions',
-    termsUrl: 'https://threatenedspeciesinitiative.com/',
-  };
-
-  bundles: Bundle[] = [
-    {
-      id: 'data-portal-galaxy',
-      name: 'Data Portal and Galaxy',
-      description: 'Bundle includes:',
-      logoUrls: ['/assets/bpa-logo.png', '/assets/galaxy-logo.png'],
-      listItems: [
-        'The <span class="font-medium">Data Portal</span> public data access',
-        'The <span class="font-medium">Galaxy Australia</span> data access',
-        'Easily import data into Galaxy from the Data Portal',
-      ],
-      services: [this.bpaBundleService, this.galaxyBundleService],
-    },
-    {
-      id: 'tsi',
-      name: 'Threatened Species Initiative',
-      description: 'Bundle includes:',
-      logoUrls: ['/assets/tsi-logo.jpg'],
-      listItems: [
-        'The <span class="font-medium">Data Portal</span> and <span class="font-medium">Galaxy Australia</span> data access',
-        'TSI public and restricted datasets in the Data Portal',
-        'Galaxy Australia 2TB storage space, high memory nodes, workflows and tools',
-        'Easily import data into Galaxy from the Data Portal',
-      ],
-      services: [
-        this.tsiBundleService,
-        this.bpaBundleService,
-        this.galaxyBundleService,
-      ],
-    },
-    {
-      id: 'fungi',
-      name: "Fungi Functional 'Omics",
-      description: '',
-      logoUrls: ['/assets/aff-logo.png'],
-      listItems: [],
-      disabled: true,
-      services: [],
-    },
-  ];
+  bundles: Bundle[] = BUNDLES;
 
   bundleForm: FormGroup = this.formBuilder.group({
     selectedBundle: ['', Validators.required],
