acf72af34642c9defa49a4ace4008aabbd5cac0f
Minh Vu <minh@biocommons.org.au>
2025-09-15 09:08:39 +1000

    fix: update error notification background color and restore backend URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,7 @@ export const environment = {
     redirectUri: window.location.origin,
     audience: 'https://dev-bc.au.auth0.com/api/v2/',
     scope: 'read:current_user update:current_user_metadata',
-    // backend: 'https://aaibackend.test.biocommons.org.au',
-    backend: 'http://localhost:8000',
+    backend: 'https://aaibackend.test.biocommons.org.au',
   },
   recaptcha: {
     siteKeyV2: '6LdR15krAAAAACZ5Lfx8yj2yBJ-whWNBTICNfwaG',
