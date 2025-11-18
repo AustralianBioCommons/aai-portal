e840a8d943c3d5958a324e9f76aa963e3d0eed64
marius-mather <marius.mather@sydney.edu.au>
2025-11-18 14:51:51 +1100

    feat: inline password change field

---
+++ src/app/shared/components/password-edit-field/password-edit-field.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasswordEditFieldComponent } from './password-edit-field.component';
+
+describe('PasswordEditFieldComponent', () => {
+  let component: PasswordEditFieldComponent;
+  let fixture: ComponentFixture<PasswordEditFieldComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PasswordEditFieldComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PasswordEditFieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
