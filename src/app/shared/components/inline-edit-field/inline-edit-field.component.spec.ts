442aee4d1f044b5d03f2a7cc610b374b950be132
marius-mather <marius.mather@sydney.edu.au>
2025-11-20 10:49:44 +1100

    test: set a value for inline-edit-field when testing

--- src/app/shared/components/inline-edit-field/inline-edit-field.component.spec.ts
+++ src/app/shared/components/inline-edit-field/inline-edit-field.component.spec.ts
@@ -13,6 +13,7 @@ describe('InlineEditFieldComponent', () => {
 
     fixture = TestBed.createComponent(InlineEditFieldComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('value', 'Initial Value');
     fixture.detectChanges();
   });
 
