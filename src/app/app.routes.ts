6658b665453abd4d881496fb169dc345c06d575a
marius-mather <marius.mather@sydney.edu.au>
2026-03-19 17:07:06 +1100

    refactor: create routes with a function for easier testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -85,7 +85,6 @@ export function createRoutes(env: EnvironmentConfig): Routes {
         },
       ],
     },
-    // Standalone routes without DefaultLayoutComponent
     ...sbpRoutes,
     {
       path: 'migration',
